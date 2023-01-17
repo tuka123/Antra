f17282f0bc1dbd4314190724f31174284ceb1706
tuka123 <52133751+tuka123@users.noreply.github.com>
2023-01-17 18:25:19 +0530

    Add files via upload

---
+++ Dogg.java
@@ -0,0 +1,40 @@
+
+//Add an equals method to this class that returns true if the current Dog
+//and passed Dog have the same name. The code should print false
+//twice then true twice.
+
+
+package All_Code;
+
+public class Dogg {
+
+    private String name;
+
+    public Dogg(String name) {
+        this.name = name;
+    }
+
+    public boolean equals(Object other) {
+        if (this == other) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+        {
+// ADD CODE HERE
+        }
+        public static void main (String[]args)
+        {
+            Dogg d1 = new Dogg("Rufus");
+            Dogg d2 = new Dogg("Sally");
+            Dogg d3 = new Dogg("Rufus");
+            Dogg d4 = d3;
+            System.out.println(d1.equals(d2));
+            System.out.println(d2.equals(d3));
+            System.out.println(d1.equals(d3));
+            System.out.println(d3.equals(d4));
+        }
+    }
+
